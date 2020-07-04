1a843d22aed19d3e2f72f8753e55cda4a9140f21
ChronisG <thegamedevelopergame@gmail.com>
2020-07-04 01:26:36 +0100

    add skill model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
+# Controller for handing home, contact, and about pages
 class PagesController < ApplicationController
   def home
+    @posts = Blog.all
+    @skills = Skill.all
   end
 
-  def about
-  end
+  def about; end
 
-  def contact
-  end
+  def contact; end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_03_050927) do
+ActiveRecord::Schema.define(version: 2020_07_04_000926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_07_03_050927) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "mastery_percentage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

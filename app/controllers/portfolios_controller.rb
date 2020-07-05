dd54f2995829085993a00cc7aa0155c133ee4fc1
ChronisG <thegamedevelopergame@gmail.com>
2020-07-05 14:17:57 +0100

    add edit and update functionality for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,18 +4,35 @@ class PortfoliosController < ApplicationController
   end
 
   def new
-    @new_item = Portfolio.new
+    @portfolio_item = Portfolio.new
   end
 
   def create
-    @new_item = Portfolio.new(params.require(:portfolio).permit(:title, :tagline, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :tagline, :body))
 
     respond_to do |format|
-      if @new_item.save
+      if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'A new portfolio item was created!' }
       else
         format.html { render :new }
       end
     end
   end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :tagline, :body))
+        format.html { redirect_to portfolios_path, notice: 'Item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
 end

ef2be645a1bba13be9590bca0bf864d683b63422
ChronisG <thegamedevelopergame@gmail.com>
2020-07-05 11:25:51 +0100

    add new & create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,20 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @new_item = Portfolio.new
+  end
+
+  def create
+    @new_item = Portfolio.new(params.require(:portfolio).permit(:title, :tagline, :body))
+
+    respond_to do |format|
+      if @new_item.save
+        format.html { redirect_to portfolios_path, notice: 'A new portfolio item was created!' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
